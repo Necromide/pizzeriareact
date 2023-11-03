715d3c2da44a467550a35d62846d12b334d39aae
Necromide <wilhelm.goerlitz95@gmail.com>
2023-11-03 15:47:18 +0100

    Klassen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
+import Speisekarte from './klassen/speisekarte';
 
 type PizzaItem = 'Salami' | 'Vegetarisch' | 'Spinat-Hühnchen';
-
-interface PizzaProps {
-    name: PizzaItem;
-    price: string;
-    addToCart: () => void;
-}
 
 interface CartProps {
     cart: PizzaItem[];
@@ -31,41 +26,8 @@ function App() {
                 <h1>Codebasis Musterpizzeria</h1>
                 <p>Musterstraße 123, 12345 Musterstadt</p>
             </header>
-
-            <nav>
-                <ul>
-                    <li>Startseite</li>
-                    <li>Speisekarte</li>
-                    <li>Bäcker</li>
-                    <li>Lieferant</li>
-                    <li>Kontakt</li>
-                </ul>
-            </nav>
-
-            <section>
-                <h2>Unsere Speisekarte</h2>
-                <Pizza
-                    name="Salami"
-                    price="8.49€"
-                    addToCart={() => addItemToCart('Salami')}
-                />
-                <Pizza
-                    name="Vegetarisch"
-                    price="10.99€"
-                    addToCart={() => addItemToCart('Vegetarisch')}
-                />
-                <Pizza
-                    name="Spinat-Hühnchen"
-                    price="11.99€"
-                    addToCart={() => addItemToCart('Spinat-Hühnchen')}
-                />
-            </section>
-
-            <section>
-                <h2>Ihr Warenkorb</h2>
-                <Cart cart={cart} removeItem={removeItemFromCart} />
-            </section>
-
+            <Speisekarte addToCart={addItemToCart} />
+            <Cart cart={cart} removeItem={removeItemFromCart} />
             <footer>
                 © 2023 Codebasis Musterpizzeria - Alle Rechte vorbehalten.
             </footer>
@@ -73,28 +35,18 @@ function App() {
     );
 }
 
-const Pizza: React.FC<PizzaProps> = ({ name, price, addToCart }) => {
-    return (
-        <div>
-            <h3>{name}</h3>
-            <p>Preis: {price}</p>
-            <button onClick={addToCart}>In den Warenkorb</button>
-        </div>
-    );
-}
-
 const Cart: React.FC<CartProps> = ({ cart, removeItem }) => {
-    let totalPrice = 0; // Die Preislogik muss noch implementiert werden
     return (
-        <div>
+        <section>
+            <h2>Ihr Warenkorb</h2>
             {cart.map((item, index) => (
                 <div key={index}>
                     {item} x1
                     <button onClick={() => removeItem(item)}>Entfernen</button>
                 </div>
             ))}
-            <p>Gesamtpreis: {totalPrice}€</p>
-        </div>
+            <p>Gesamtpreis: {/* Hier müsste die Preislogik implementiert werden */}€</p>
+        </section>
     );
 }
 
